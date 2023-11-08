6e85d9c2e3bb346de213d26d48d316ac826badb1
tarek12mig <tarekdesign@protonmail.com>
2023-11-08 20:49:42 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Remove the 'active' class from all links
+        document.querySelectorAll('a[href^="#"]').forEach(link => {
+            link.classList.remove('active');
+        });
+
+        // Add the 'active' class to the clicked link
+        this.classList.add('active');
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
